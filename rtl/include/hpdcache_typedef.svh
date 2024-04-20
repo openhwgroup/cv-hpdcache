c1c9762077e4922d5209d96be635f7c2eefb66c9
Cesar Fuguet <cesar.fuguettortolero@cea.fr>
2024-04-21 00:50:32 +0200

    macros: add utility macros to define types in the interface

--- rtl/include/hpdcache_typedef.svh
+++ rtl/include/hpdcache_typedef.svh
@@ -26,79 +26,95 @@
 `ifndef __HPDCACHE_TYPEDEF_SVH__
 `define __HPDCACHE_TYPEDEF_SVH__
 
-`define HPDCACHE_DECL_MEM_REQ_T(addr_t, id_t) \
+`define HPDCACHE_DECL_MEM_REQ_T(__addr_t, __id_t) \
     struct packed { \
-        addr_t                                mem_req_addr; \
+        __addr_t                              mem_req_addr; \
         hpdcache_pkg::hpdcache_mem_len_t      mem_req_len; \
         hpdcache_pkg::hpdcache_mem_size_t     mem_req_size; \
-        id_t                                  mem_req_id; \
+        __id_t                                mem_req_id; \
         hpdcache_pkg::hpdcache_mem_command_e  mem_req_command; \
         hpdcache_pkg::hpdcache_mem_atomic_e   mem_req_atomic; \
         logic                                 mem_req_cacheable; \
     }
 
-`define HPDCACHE_DECL_MEM_RESP_R_T(id_t, data_t) \
+`define HPDCACHE_DECL_MEM_RESP_R_T(__id_t, __data_t) \
     struct packed { \
         hpdcache_pkg::hpdcache_mem_error_e    mem_resp_r_error; \
-        id_t                                  mem_resp_r_id; \
-        data_t                                mem_resp_r_data; \
+        __id_t                                mem_resp_r_id; \
+        __data_t                              mem_resp_r_data; \
         logic                                 mem_resp_r_last; \
     }
 
-`define HPDCACHE_DECL_MEM_REQ_W_T(data_t, be_t) \
+`define HPDCACHE_DECL_MEM_REQ_W_T(__data_t, __be_t) \
     struct packed { \
-        data_t                                mem_req_w_data; \
-        be_t                                  mem_req_w_be; \
+        __data_t                              mem_req_w_data; \
+        __be_t                                mem_req_w_be; \
         logic                                 mem_req_w_last; \
     }
 
-`define HPDCACHE_DECL_MEM_RESP_W_T(id_t) \
+`define HPDCACHE_DECL_MEM_RESP_W_T(__id_t) \
     struct packed { \
         logic                                 mem_resp_w_is_atomic; \
         hpdcache_pkg::hpdcache_mem_error_e    mem_resp_w_error; \
-        id_t                                  mem_resp_w_id; \
+        __id_t                                mem_resp_w_id; \
     }
 
-`define HPDCACHE_TYPEDEF_MEM_REQ_T(__name__, addr_t, id_t) \
-    typedef `HPDCACHE_DECL_MEM_REQ_T(addr_t, id_t) __name__
+`define HPDCACHE_TYPEDEF_MEM_ATTR_T(__addr_t, __id_t, __data_t, __be_t, __params) \
+    typedef logic [  __params.u.memAddrWidth-1:0] __addr_t; \
+    typedef logic [    __params.u.memIdWidth-1:0] __id_t; \
+    typedef logic [  __params.u.memDataWidth-1:0] __data_t; \
+    typedef logic [__params.u.memDataWidth/8-1:0] __be_t
 
-`define HPDCACHE_TYPEDEF_MEM_RESP_R_T(__name__, id_t, data_t) \
-    typedef `HPDCACHE_DECL_MEM_RESP_R_T(id_t, data_t) __name__
+`define HPDCACHE_TYPEDEF_MEM_REQ_T(__name__, __addr_t, __id_t) \
+    typedef `HPDCACHE_DECL_MEM_REQ_T(__addr_t, __id_t) __name__
 
-`define HPDCACHE_TYPEDEF_MEM_REQ_W_T(__name__, data_t, be_t) \
-    typedef `HPDCACHE_DECL_MEM_REQ_W_T(data_t, be_t) __name__
+`define HPDCACHE_TYPEDEF_MEM_RESP_R_T(__name__, __id_t, __data_t) \
+    typedef `HPDCACHE_DECL_MEM_RESP_R_T(__id_t, __data_t) __name__
 
-`define HPDCACHE_TYPEDEF_MEM_RESP_W_T(__name__, id_t) \
-    typedef `HPDCACHE_DECL_MEM_RESP_W_T(id_t) __name__
+`define HPDCACHE_TYPEDEF_MEM_REQ_W_T(__name__, __data_t, __be_t) \
+    typedef `HPDCACHE_DECL_MEM_REQ_W_T(__data_t, __be_t) __name__
 
-`define HPDCACHE_DECL_REQ_T(offset_t, data_t, be_t, sid_t, tid_t, tag_t) \
+`define HPDCACHE_TYPEDEF_MEM_RESP_W_T(__name__, __id_t) \
+    typedef `HPDCACHE_DECL_MEM_RESP_W_T(__id_t) __name__
+
+`define HPDCACHE_DECL_REQ_T(__offset_t, __data_t, __be_t, __sid_t, __tid_t, __tag_t) \
     struct packed { \
-        offset_t                          addr_offset; \
-        data_t                            wdata; \
+        __offset_t                        addr_offset; \
+        __data_t                          wdata; \
         hpdcache_pkg::hpdcache_req_op_t   op; \
-        be_t                              be; \
+        __be_t                            be; \
         hpdcache_pkg::hpdcache_req_size_t size; \
-        sid_t                             sid; \
-        tid_t                             tid; \
+        __sid_t                           sid; \
+        __tid_t                           tid; \
         logic                             need_rsp; \
         logic                             phys_indexed; \
-        tag_t                             addr_tag; \
+        __tag_t                           addr_tag; \
         hpdcache_pkg::hpdcache_pma_t      pma; \
     }
 
-`define HPDCACHE_TYPEDEF_REQ_T(__name__, offset_t, data_t, be_t, sid_t, tid_t, tag_t) \
-    typedef `HPDCACHE_DECL_REQ_T(offset_t, data_t, be_t, sid_t, tid_t, tag_t) __name__
+`define HPDCACHE_TYPEDEF_REQ_ATTR_T(__offset_t, __word_t, __word_be_t, __data_t, __be_t, __sid_t, __tid_t, __tag_t, __params) \
+    typedef logic       [         __params.tagWidth-1:0] __tag_t; \
+    typedef logic       [      __params.u.wordWidth-1:0] __word_t; \
+    typedef logic       [    __params.u.wordWidth/8-1:0] __word_be_t; \
+    typedef logic       [   __params.reqOffsetWidth-1:0] __offset_t; \
+    typedef __word_t    [       __params.u.reqWords-1:0] __data_t; \
+    typedef __word_be_t [       __params.u.reqWords-1:0] __be_t; \
+    typedef logic       [  __params.u.reqSrcIdWidth-1:0] __sid_t; \
+    typedef logic       [__params.u.reqTransIdWidth-1:0] __tid_t
 
-`define HPDCACHE_DECL_RSP_T(data_t, sid_t, tid_t) \
+`define HPDCACHE_TYPEDEF_REQ_T(__name__, __offset_t, __data_t, __be_t, __sid_t, __tid_t, __tag_t) \
+    typedef `HPDCACHE_DECL_REQ_T(__offset_t, __data_t, __be_t, __sid_t, __tid_t, __tag_t) __name__
+
+`define HPDCACHE_DECL_RSP_T(__data_t, __sid_t, __tid_t) \
     struct packed { \
-        data_t   rdata; \
-        sid_t    sid; \
-        tid_t    tid; \
+        __data_t rdata; \
+        __sid_t  sid; \
+        __tid_t  tid; \
         logic    error; \
         logic    aborted; \
     }
 
-`define HPDCACHE_TYPEDEF_RSP_T(__name__, data_t, sid_t, tid_t) \
-    typedef `HPDCACHE_DECL_RSP_T(data_t, sid_t, tid_t) __name__
+`define HPDCACHE_TYPEDEF_RSP_T(__name__, __data_t, __sid_t, __tid_t) \
+    typedef `HPDCACHE_DECL_RSP_T(__data_t, __sid_t, __tid_t) __name__
 
 `endif //  __HPDCACHE_TYPEDEF_SVH__
